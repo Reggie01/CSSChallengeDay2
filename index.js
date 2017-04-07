6599b34608503d56edbd8ad6c7d83b1ea60e6280
Reggie S <reggie@bgcreatives.com>
2017-04-07 16:57:10 -0400

    day 2
    Day 2 implentation of 100 days css challenge. Review readme for state of app.

---
+++ index.js
@@ -0,0 +1,32 @@
+﻿
+  // Polyfill
+  if (!String.prototype.trim) {
+    String.prototype.trim = function () {
+      return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+    };
+  }
+  
+  var innerContainer = document.getElementById( "innerContainer" );
+  
+  function toggleAnimation( classname ) {
+    var classes = classname;
+	if( classes.indexOf( "animate" ) != -1 ){
+		 classes = classes.replace( "animate", "");
+	} else {
+		classes = classes + " animate";
+	}
+	return classes.trim();
+  }
+  
+  function handler( e ) {
+	  console.log( "clicked" );  
+	  let children = Array.prototype.slice.call( e.target.children );
+	  children.map( function( child ) {
+		  // console.log( "Before: " + child.className );
+		  child.className = toggleAnimation( child.className );
+		  // console.log( "After: " + child.className );
+	  })
+	  
+  }
+
+innerContainer.addEventListener( "click", handler, false );
---
+++ readme.md
@@ -0,0 +1,2 @@
+﻿[100 days of css](http://100dayscss.com/) had trouble reimplementing this design. The rotation after the translation of the equal elements is not in place. Also, had issue with z-index. The div.inner-container has a higher z-index than it's inner elements. User will need to click directly below equals sign for css transformation to start.
+Will have to look at solution to figure out rest.
